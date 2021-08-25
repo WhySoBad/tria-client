de071b4039fc050b4ed4265f92cc85727a9b2f39
Corsin <hermcors@gmail.com>
2021-08-25 22:29:08 +0200

    new api url

--- src/util/config.ts
+++ src/util/config.ts
@@ -1,6 +1,6 @@
 /** @internal */
 export const config = {
-  apiUrl: 'http://194.195.246.128:3000', //ip of the api server
+  apiUrl: 'https://api.tria.chat', //ip of the api server
   eventDelay: 100, //the delay is required to gather some time to update changes
   maxListenerCount: 0, //disables max listener warning
 };
